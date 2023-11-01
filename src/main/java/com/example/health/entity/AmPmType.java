8af7b8208a1fc79c76de986059988c4b3efd8ee9
hyunji <qkrguswl18@naver.com>
2023-11-01 16:54:46 +0900

    remove: 불필요한 코드 제거

--- src/main/java/com/example/health/entity/AmPmType.java
+++ src/main/java/com/example/health/entity/AmPmType.java
@@ -1,6 +1,5 @@
 package com.example.health.entity;
 
-import com.example.health.exception.EnumNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,11 +14,4 @@ public enum AmPmType {
 
     private String type;
     private String name;
-
-    public static AmPmType of(String type){
-        return Stream.of(AmPmType.values())
-                .filter(v -> v.getType().equals(type))
-                .findAny()
-                .orElseThrow(() -> new EnumNotFoundException(String.format("파라미터 %s에 해당하는 데이터가 존재하지 않습니다. 파라미터는 AM, PM 둘 중 하나만 사용 가능합니다.", type)));
-    }
 }
