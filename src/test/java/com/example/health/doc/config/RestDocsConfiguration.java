2321627e04c85fd24cc82c4baf17377d04aaa0cc
hyunji <qkrguswl18@naver.com>
2023-11-06 14:06:38 +0900

    chore: Spring Rest Docs 적용

---
+++ src/test/java/com/example/health/doc/config/RestDocsConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.health.doc.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler write() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                preprocessRequest(
+                        modifyUris()
+                                .scheme("https")
+                                .host("docs.api.com")
+                                .removePort(),
+                        prettyPrint()),
+                preprocessResponse(prettyPrint())
+        );
+    }
+}
